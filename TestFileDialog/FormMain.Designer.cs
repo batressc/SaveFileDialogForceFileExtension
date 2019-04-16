2c93f6d585f55106df28ecf7fbbea211a433f733
Luis Gustavo Fernández Batres <elclon_fernanbrans@hotmail.com>
2019-04-16 13:49:34 -0600

    Agregar archivos de proyecto.

---
+++ TestFileDialog/FormMain.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TestFileDialog {
+    partial class FormMain {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(412, 23);
+            this.buttonSaveFile.TabIndex = 0;
+            this.buttonSaveFile.Text = "&Save file";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFileName.Location = new System.Drawing.Point(12, 42);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(412, 20);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 77);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Name = "FormMain";
+            this.Text = "Save file dialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.TextBox textBoxFileName;
+    }
+}
+
---
+++ TestFileDialog/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFileDialog {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e) {
+            SaveFileDialog dialog = new SaveFileDialog {
+                AddExtension = true,
+                DefaultExt = "txt",
+                Filter = "Text files (*.txt)|*.txt|XML files (*.xml)|*.xml|Multidotted (*.abc.def)|*.abc.def",
+                OverwritePrompt = true
+            };
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                textBoxFileName.Text = dialog.FileName + " | " + dialog.FileNameForceExtension();
+            }
+        }
+    }
+}
